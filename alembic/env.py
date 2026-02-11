b3a420fe37692927c46279baecedb18b7275cc5b
Elvin cooper <ing.elvin01cooper@gmail.com>
2026-02-11 16:06:11 -0400

    configuracion alembic lista

--- alembic/env.py
+++ alembic/env.py
@@ -86,12 +86,6 @@ def run_migrations_online() -> None:
 
     connectable = engine
 
-    # connectable = engine_from_config(
-    #     config.get_section(config.config_ini_section, {}),
-    #     prefix="sqlalchemy.",
-    #     poolclass=pool.NullPool,
-    # )
-
     with connectable.connect() as connection:
         context.configure(
             connection=connection, target_metadata=target_metadata
